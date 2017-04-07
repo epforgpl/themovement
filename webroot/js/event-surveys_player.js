c52eb31b54ae8f7c74bb777b58e9d4474da87fb6
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2017-04-07 08:48:50 +0200

    surveys

--- webroot/js/event-surveys_player.js
+++ webroot/js/event-surveys_player.js
@@ -9,6 +9,9 @@ _SURVEYS_PLAYER.prototype = {
 	
 	current_question: null,
 	current_answer: null,
+	current_other: null,
+	
+	other_block: false,
 	
 	init: function(div){
 		
@@ -44,73 +47,112 @@ _SURVEYS_PLAYER.prototype = {
 		
 		var that = this;
 		
-		$.get('/events/' + this.event_id + '/getActiveQuestion.json').always(function(data){
-			
-			that.block.removeClass('loading');
-			
-			if( data && data.id ) {
-				
-				if( that.current_question && ( that.current_question.id != data.id ) ) {
-					that.current_answer = null;
-				}
-				
-				that.block.data('id', data.id);
-				that.block.removeClass('empty');
-				that.block.find('h2').html( data.text );
-				
-				var answers_ul = that.block.find('.answers').html('');
-				
-				for( var i=0; i<data.surveys_answers.length; i++ ) {
+		if( !this.other_block ) {
 					
-					var ans = data.surveys_answers[i];
-					var li = $('<li><label><input type="radio" name="ans" value="' + ans.id + '" />' + ans.text + '</label></li>');
+			$.get('/events/' + this.event_id + '/getActiveQuestion.json').always(function(data){
+				
+				that.block.removeClass('loading');
+				
+				if( data && data.id ) {
 					
-					var inp = li.find('input').prop('checked', false);
+					if( that.current_question && ( that.current_question.id != data.id ) ) {
+						that.current_answer = null;
+					}
 					
-					if( data.result ) {
+					that.block.data('id', data.id);
+					that.block.removeClass('empty');
+					that.block.find('h2').html( data.text );
+					
+					var answers_ul = that.block.find('.answers').html('');
+					
+					for( var i=0; i<data.surveys_answers.length; i++ ) {
 						
-						inp.addClass('disabled').prop('disabled', 'disabled');
+						var ans = data.surveys_answers[i];
+						var li = $('<li><label><input type="radio" name="ans" value="' + ans.id + '" />' + ans.text + '</label></li>');
 						
-						if( data.result.answer_id == ans.id ) {
+						var inp = li.find('input').prop('checked', false);
+						
+						if( data.result ) {
+							
+							inp.addClass('disabled').prop('disabled', 'disabled');
+							
+							if( data.result.answer_id == ans.id ) {
+								inp.prop('checked', true);
+							}
+							
+						}
+						
+						inp.change(function(event){
+							var _inp = $(event.target).closest('input');
+							that.current_answer = _inp.val();
+						});
+						
+						if( that.current_answer && ( that.current_answer == ans.id ) ) {
 							inp.prop('checked', true);
 						}
 						
+						answers_ul.append(li);
+						
 					}
 					
-					inp.change(function(event){
-						var _inp = $(event.target).closest('input');
-						that.current_answer = _inp.val();
-					});
-					
-					if( that.current_answer && ( that.current_answer == ans.id ) ) {
-						inp.prop('checked', true);
+					if( data.others ) {
+						
+						var li = $('<li><input type="text" class="form-control inp-other" style="width: 90%;" name="other" placeholder="Other..." /></li>');
+						
+						var inp = li.find('input');
+						
+						inp.focus(function(event){
+							
+							that.other_block = true;
+							
+							that.block.find('input[type=radio]').each(function(){
+								$(this).prop('checked', false);
+							});
+							that.current_answer = false;
+							
+						}).blur(function(event){
+							
+							that.current_other = $(event.target).closest('input').val();
+							that.other_block = false;
+							
+						});
+						
+						
+						if( that.current_other )
+							inp.val(that.current_other);
+						
+						
+						
+						answers_ul.append(li);
+	
+						
 					}
 					
-					answers_ul.append(li);
+					if( data.result ) {
+						that.vote_button.addClass('disable').prop('disabled', 'disabled');
+						that.status.show();
+					} else {
+						that.vote_button.removeClass('disable').prop('disabled', false);
+						that.status.hide();
+					}
 					
-				}
-				
-				if( data.result ) {
-					that.vote_button.addClass('disable').prop('disabled', 'disabled');
-					that.status.show();
+					current_question = data;
+					
 				} else {
-					that.vote_button.removeClass('disable').prop('disabled', false);
-					that.status.hide();
+					
+					that.block.addClass('empty');
+					
 				}
 				
-				current_question = data;
 				
-			} else {
 				
-				that.block.addClass('empty');
-				
-			}
-			
-			setTimeout(function(){
-				that.checkQuestion();
-			}, that.timeout);
-			
-		});
+			});
+		
+		}
+		
+		setTimeout(function(){
+			that.checkQuestion();
+		}, that.timeout);
 		
 	},
 	
@@ -119,6 +161,7 @@ _SURVEYS_PLAYER.prototype = {
 		var that = this;
 		
 		var answer = false;
+		var answer_other = false;
 		this.div.find('.answers input').each(function(){
 			var inp = $(this);
 			if( inp.prop('checked') ) {
@@ -126,8 +169,15 @@ _SURVEYS_PLAYER.prototype = {
 			}
 		});
 		
-		if( answer !== false ) {
+		answer_other = this.div.find('.answers .inp-other').val();
 		
+		console.log('save', answer, answer_other);
+		
+		if( (answer !== false) || answer_other ) {
+			
+			if( !answer )
+				answer = 0;
+			
 			this.vote_button.addClass('disable').prop('disabled', 'disabled');
 			this.div.find('.answers input').addClass('disable').prop('disabled', 'disabled');
 			this.progress.css({visibility: 'visible'});
@@ -135,7 +185,8 @@ _SURVEYS_PLAYER.prototype = {
 						
 			$.post(this.action + '.json', {
 				question_id: that.block.data('id'),
-				answer_id: answer
+				answer_id: answer,
+				other: answer_other,
 			}, function(data){
 				
 				that.status.slideDown();
